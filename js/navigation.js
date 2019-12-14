e8e744e03734a826b1f54402878fa9a7b6f9e3ec
LucianABC <louabecasis@gmail.com>
2019-12-14 13:58:13 -0300

    probelmas con query selector

---
+++ js/navigation.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector("#cardNav");
+
+const table = document.querySelector("#navTables");
+
+table.addEventListener("click", ()=>{
+    table.classList.add("active");
+});
